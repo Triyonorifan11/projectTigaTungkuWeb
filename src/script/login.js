5de51d001fc400f7bc807651722752b0cbae36ce
Triyono Rifan <triyonorifan01@gmail.com>
2022-12-04 14:33:32 +0700

    auto redirect after login in login page

--- src/script/login.js
+++ src/script/login.js
@@ -3,8 +3,17 @@ import 'regenerator-runtime';
 import '../style/style.css';
 import loginAdmin from './utils/loginAdmin';
 
+function checkLogin() {
+  const localStr = localStorage.getItem('TigaTungku');
+  const dataLocal = JSON.parse(localStr);
+  if (dataLocal === 'tigatungku.id@gmail.com') {
+    window.location.href = './';
+  }
+}
+
 window.addEventListener('load', () => {
   document.getElementById('spinner').classList.add('show');
+  checkLogin();
   setTimeout(() => {
     document.getElementById('spinner').classList.remove('show');
     loginAdmin.init();
