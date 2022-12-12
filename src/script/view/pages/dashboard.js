cca94867be7f21e944c2b6b074aa66eddba2608d
Triyono Rifan <triyonorifan01@gmail.com>
2022-12-12 16:23:48 +0700

    update view dashboard

--- src/script/view/pages/dashboard.js
+++ src/script/view/pages/dashboard.js
@@ -2,39 +2,21 @@ const dashboard = {
   async render() {
     return `
     <div class="row g-4">
-        <div class="col-sm-6 col-xl-3">
-            <div class="bg-orangelight rounded d-flex align-items-center justify-content-between p-4">
-                <i class="fa fa-chart-line fa-3x text-orange"></i>
-                <div class="ms-3">
-                    <p class="mb-2 text-orange">Today Sale</p>
-                    <h6 class="mb-0 text-orange">$1234</h6>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6 col-xl-3">
+        <div class="col-sm-6 col-xl-6">
             <div class="bg-orangelight rounded d-flex align-items-center justify-content-between p-4">
                 <i class="fa fa-chart-bar fa-3x text-orange"></i>
                 <div class="ms-3">
-                    <p class="mb-2 text-orange">Total Sale</p>
-                    <h6 class="mb-0 text-orange">$1234</h6>
+                    <h4 class="mb-2 text-orange">Total Produk</h4>
+                    <h5 class="mb-0 text-orange">8</h5>
                 </div>
             </div>
         </div>
-        <div class="col-sm-6 col-xl-3">
-            <div class="bg-orangelight rounded d-flex align-items-center justify-content-between p-4">
-                <i class="fa fa-chart-area fa-3x text-orange"></i>
-                <div class="ms-3">
-                    <p class="mb-2 text-orange">Today Revenue</p>
-                    <h6 class="mb-0 text-orange">$1234</h6>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6 col-xl-3">
+        <div class="col-sm-6 col-xl-6">
             <div class="bg-orangelight rounded d-flex align-items-center justify-content-between p-4">
                 <i class="fa fa-chart-pie fa-3x text-orange"></i>
                 <div class="ms-3">
-                    <p class="mb-2 text-orange">Total Revenue</p>
-                    <h6 class="mb-0 text-orange">$1234</h6>
+                    <h4 class="mb-2 text-orange">Total Testimoni</h4>
+                    <h5 class="mb-0 text-orange">$1234</h6>
                 </div>
             </div>
         </div>
