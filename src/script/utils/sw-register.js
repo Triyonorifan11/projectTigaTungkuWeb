3f95b1257c8b21aa73f748688dfa0cd972993ff9
Triyono Rifan <triyonorifan01@gmail.com>
2022-12-13 20:25:40 +0700

    update sw

--- src/script/utils/sw-register.js
+++ src/script/utils/sw-register.js
@@ -6,7 +6,6 @@ const swRegister = async () => {
 
   try {
     await navigator.serviceWorker.register('./sw.bundle.js');
-    console.log('Service worker registered');
   } catch (error) {
     console.log('Failed to register service worker', error);
   }
